14c129abee15cf6081b39ac06faf581a2b1906c7
Matt20144 <ahmadmamaddddd@gmail.com>
2024-08-28 01:02:21 +0000

    updated to old version in google chrome

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 #print("hello, world")
 @app.route("/")  # this means whenever root gets called then print hello_world
-
 def home():
-  return render_template('ordernow.html')
+  return render_template('home.html')
 
 @app.route("/contact")
 def contact():
   return render_template('contactus.html')
+
+@app.route("/order")
+def order():
+  return render_template("ordernow.html")
+
+@app.route("/menu")
+def menu():
+  return render_template("ourmenu.html")
 
 
 
