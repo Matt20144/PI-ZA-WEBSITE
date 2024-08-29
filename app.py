e535656662c9e188db3e255e7b2d3a995036a65b
Matt20144 <ahmadmamaddddd@gmail.com>
2024-08-29 04:16:11 +0000

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #print("hello, world")
 @app.route("/")  # this means whenever root gets called then print hello_world
 def home():
-  return render_template('home.html')
+  return render_template('drinks.html')
 
 @app.route("/contact")
 def contact():
@@ -17,6 +17,22 @@ def order():
 @app.route("/menu")
 def menu():
   return render_template("ourmenu.html")
+@app.route("/pies")
+def pies():
+  return render_template("pies.html")
+
+@app.route("/rolls")
+def rolls():
+  return render_template("rolls.html")
+
+@app.route("/sweets")
+def sweets():
+  return render_template("sweets.html")
+
+@app.route("/drinks")
+def drinks():
+  return render_template("drinks.html")
+
 
 
 
